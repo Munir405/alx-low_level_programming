cea691cf0164707675190404c006acf4cda1891a
akubosylva <akubosylva@gmail.com>
2022-09-08 07:16:04 -0700

    Printf function in C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Point of entry
+ * Return: zero (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
