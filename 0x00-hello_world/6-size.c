fda229a573dcc023f0d905889f4e6ed71f364116
akubosylva <akubosylva@gmail.com>
2022-09-08 08:17:40 -0700

    C Program for Size of data

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,21 +2,16 @@
 
 /**
  * main - Point of entry
+ * Description: To determine the size of data type on various machine
  * Return: zero (Success)
  */
 
 int main(void)
 {
-	int int_type;
-	char char_type;
-	float float_type;
-	long int long_int_type;
-	long long int long_long_int_type;
-
-	printf("Size of a char: %lu byte(s)\n", sizeof(char_type));
-	printf("Size of an int: %lu byte(s)\n", sizeof(int_type));
-	printf("Size of a long: %lu byte(s)\n", sizeof(long_int_type));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(long_long_int_type));
-	printf("Size of a float: %lu byte(s)\n", sizeof(float_type));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
