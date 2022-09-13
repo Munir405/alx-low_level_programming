22b84f8174db57c57bd14885ea81ab53f95eec82
akubosylva <akubosylva@gmail.com>
2022-09-13 09:05:34 -0700

    Add header file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,92 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: character to print to stdout
+ * Return: 1 if success, -1 if error
+ */
+
+int _putchar(char c);
+
+/**
+ * print_alphabet - print lowercase alphabet
+ */
+
+void print_alphabet(void);
+
+/**
+ * print_alphabet_x10 - print lowercase alphabet ten times
+ */
+
+void print_alphabet_x10(void);
+
+/**
+ * _islower - checks for lowercase
+ * @c: character to check
+ * Return: int
+ */
+
+int _islower(int c);
+
+/**
+ * _isalpha - prints alphabet character
+ * @c: character to print
+ * Return: 1 if lower or uppercase and 0 otherwise
+ */
+
+int _isalpha(int c);
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: number tpo check the sign
+ * Return: 1 if > 0, 0 if 0, -1 if < 0
+ */
+
+int print_sign(int n);
+
+/**
+ * _abs - computes the absolute value of integer
+ * @int: intger to compute
+ * Return: absolute value of integer
+ */
+
+int _abs(int);
+
+/**
+ * print_last_digit - prints the last digit of integer
+ * @int: integer to print last digit of
+ * Return: last digit of integer
+ */
+
+int print_last_digit(int);
+
+/**
+ * jack_bauer - print the minutes of the day
+ * Description: print from 00:00 to 23:59
+ * Return: void
+ */
+
+void jack_bauer(void);
+
+/**
+ * times_table - print multiplication table of numbers starting from zero
+ */
+
+void times_table(void);
+
+/**
+ * add - add two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: sum
+ */
+
+int add(int a, int b);
+
+/**
+ * print_to_98 - prints number n to 98
+ * @n: integer number
+ */
+
+void print_to_98(int n);
