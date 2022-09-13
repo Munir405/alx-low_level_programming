a474c8bfb5b6eb75331645713796df7639a4f06f
akubosylva <akubosylva@gmail.com>
2022-09-13 11:30:30 -0700

    Print numbers till 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,20 +9,27 @@
 
 void print_to_98(int n)
 {
-	while (n <= 98)
+	if (n >= 98)
 	{
-		printf("%d, ", n);
-		n++;
+		while (n >= 98)
+		{
+			if (n != 98)
+				printf("%d, ", n);
+			else
+				printf("%d", n);
+			n--;
+		}
 	}
-
-	while (n >= 98)
+	else if (n < 98)
 	{
-		printf("%d, ", n);
-		n--;
+		while (n <= 98)
+		{
+			if (n != 98)
+				printf("%d, ", n);
+			else
+				printf("%d", n);
+			n++;
+		}
 	}
-
-	if (n == 98)
-		printf("%d, ", n);
-
-	printf("\n");
+	_putchar('\n');
 }
