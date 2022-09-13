e44542c270d4eeeffd6feb045215bafb4119dd0a
akubosylva <akubosylva@gmail.com>
2022-09-13 10:33:17 -0700

    Print time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,7 +14,7 @@ void jack_bauer(void)
 		while (minutes < 60)
 		{
 			_putchar('0' + (hours / 10));
-			_putchar('0' + (hours %));
+			_putchar('0' + (hours % 10));
 			_putchar(':');
 			_putchar('0' + (minutes / 10));
 			_putchar('0' + (minutes % 10));
