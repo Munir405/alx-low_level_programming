bb1a8e2109e60198e541b55c624fe1beb1febaf0
akubosylva <akubosylva@gmail.com>
2022-09-13 09:28:37 -0700

    Print alphabetx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints lowercase character in ten places
+ * Return: zero(0)
+ */
+
+void print_alphabet_x10(void)
+{
+	char letter;
+
+	int number;
+
+	for(number = 0; number <= 9; number++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
+
+	return (0);
+}
