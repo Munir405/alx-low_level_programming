8dd5fe74cb53be4b2113d3313e4ad6b583ba7bfb
akubosylva <akubosylva@gmail.com>
2022-09-14 08:07:52 -0700

    Print Remaining Days

--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -15,7 +15,7 @@ int main(void)
 
 	month = 02;
 	day = 29;
-	year = 2000;
+	year = 2427;
 	printf("Date: %02d/%02d/%04d\n", month, day, year);
 	day = convert_day(month, day);
 	print_remaining_days(month, day, year);
