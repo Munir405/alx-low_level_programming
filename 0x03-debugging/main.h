58a0b552c0df26a12bd67881caba2261a2c478ce
akubosylva <akubosylva@gmail.com>
2022-09-14 04:48:27 -0700

    Print largest number

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,21 @@
 
 #include <stdio.h>
 
+/**
+ * positive_or_negative - checks if an integer is positive,negative or zero
+ * @i: integer argument
+ */
+
 void positive_or_negative(int i);
+
+/**
+ * largest_number - returns the largest of three numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c);
 
 #endif /*MAIN_H*/
