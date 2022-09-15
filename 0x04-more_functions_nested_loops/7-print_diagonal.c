5a27d78357e3863c7b20a69ee076178624bc531f
akubosylva <akubosylva@gmail.com>
2022-09-15 07:37:00 -0700

    Print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints \ for number of times to form a diagonal
+ * @n: number of times to print
+ */
+
+void print_diagonal(int n)
+{
+	int line;
+	int space;
+
+	if (n > 0)
+	{
+		for (line = 0; line < n; line++)
+		{
+			for (space = 1; space < line; space++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
