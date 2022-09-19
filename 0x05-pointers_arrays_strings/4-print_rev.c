f77343b105da44c66c2bfe7647576b4e75d22dfb
akubosylva <akubosilver@gmail.com>
2022-09-19 18:53:06 +0100

    String reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse order followed by a new line
+ * @s: string to reverse
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+	int i;
+
+	/* find length of string and point to the last character */
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	/* print string reversed starting from the end */
+	for (i = len; i > 0; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
+}
