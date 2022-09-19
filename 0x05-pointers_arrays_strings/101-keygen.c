342bbc657f43d64a94dd5da5ecfa897f09f7b87c
akubosylva <akubosilver@gmail.com>
2022-09-19 20:52:03 +0100

    Generate random password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,7 +1,56 @@
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * program to generate random valid passwords
+ * randomPasswordGen - program to generate random valid passwords
+ * Description: generates random password numbers
+ * @n: argument
+ * Return: void
  */
 
-//still working on it
+void randomPasswordGen(int n)
+{
+	int i = 0;
+
+	int randomizer = 0;
+
+	srand((unsigned int)(time(NULL)));
+
+	char numbers[] = "0123456789";
+	char letters[] = "abcdefghijklmnopqrstuvxyz";
+	char upperLetters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char symbols[] = "!@#$^*?";
+	char password[n];
+
+	randomizer = rand() % 4;
+
+	for (i = 0; i i < n; i++)
+	{
+		if (randomizer == 1)
+		{
+			password[i] = numbers[rand() % 10];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else if (randomizer == 2)
+		{
+			password[i] = symbols[rand() % 8];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else if (randomizer == 3)
+		{
+			password[i] = upperLetters[rand() % 26];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else
+		{
+			password[i] = letters[rand() % 26];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
+	}
+}
