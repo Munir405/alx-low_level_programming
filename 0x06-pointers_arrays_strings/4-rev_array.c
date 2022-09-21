04374b73e1c3d4a4da8439ace1fd8a118e45470a
akubosylva <akubosilver@gmail.com>
2022-09-21 18:45:25 +0100

    Reverse Array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,14 +8,17 @@
 
 void reverse_array(int *a, int n)
 {
-	int tmp, begin = 0;
-	int end = n - 1;
+	int i, j, tmp;
 
-	while (begin < end)
+	i = 0;
+	j = n - 1;
+
+	while (i < j)
 	{
-		tmp = *(a + begin);
-		*(a + begin) = *(a + end);
-		*(a + end) = tmp;
-		begin++, end++;
+		tmp = a[i];
+		a[i] = a[j];
+		a[j] = tmp;
+		i++;
+		j--;
 	}
 }
