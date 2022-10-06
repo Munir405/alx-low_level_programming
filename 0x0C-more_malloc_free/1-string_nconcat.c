72e77e6fb446bb3c6d8bb2fde3fcf6aa680ce493
akubosylva <akubosilver@gmail.com>
2022-10-06 21:51:57 +0100

    Calloc Function

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -49,9 +49,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	/* concate strings */
 	for (i = 0; s1[i] != '\0'; i++)
 		ptr[i] = s1[i];
-	for (j = 0; j < num; j++)
-		ptr[i + j] = s2[j];
-	ptr[i + j] = '\0';
+	for (j = 0; j < num; j++, i++)
+		ptr[i] = s2[j];
+	ptr[i] = '\0';
 
 	return (ptr);
 }
