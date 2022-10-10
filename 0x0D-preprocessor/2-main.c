04585230d03fb1729b6f1579f8a858da22624c77
akubosylva <akubosilver@gmail.com>
2022-10-10 09:56:45 +0100

    Print name of file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints name of complied file
+ * Return: zero(success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
