a7f385fa0c22dcc13d88ec687577e25ed765f544
akubosylva <akubosilver@gmail.com>
2022-10-13 19:57:19 +0100

    Rewrite Print_buffer Function

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,36 +2,6 @@
 #include <stdio.h>
 
 /**
- * print_line - prints s bytes of a buffer
- * @c: buffer to print
- * @s: bytes of buffer to print
- * @l: line of buffer to print
- */
-
-void print_line(char *c, int s, int l)
-{
-	int j, k;
-
-	for (j = 0; j <= 9; j++)
-	{
-		if (j <= s)
-			printf("%02x", c[l * 10 + j]);
-		else
-			printf("  ");
-		if (j % 2)
-			putchar(' ');
-	}
-
-	for (k = 0; k <= s; k++)
-	{
-		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
-			putchar(c[l * 10 + k]);
-		else
-			putchar('.');
-	}
-}
-
-/**
  * print_buffer - prints a buffer
  * @b: buffer to print
  * @size: size of buffer
@@ -39,21 +9,35 @@ void print_line(char *c, int s, int l)
 
 void print_buffer(char *b, int size)
 {
-	int i;
+	int i, j, k;
 
-	for (i = 0; i <= (size - 1) / 10 && size; i++)
+	if (size <= 0)
 	{
-		printf("%08x: ", i * 10);
-		if (i < size / 10)
-		{
-			print_line(b, 9, i);
-		}
-		else
-		{
-			print_line(b, size % 10 - 1, i);
-		}
-		putchar('\n');
+		printf("\n");
+		return;
 	}
-	if (size == 0)
-		putchar('\n');
+
+	for (k = 0; k < size; k += 10)
+	{
+		j = size - k < 10 ? size - k : 10;
+		printf("%08x: ", k);/* print hex value of k */
+		for (i = 0; i < 10; i++)
+		{
+			if (i < j)
+				printf("%02x", b[k + i]);/*print hex value of letter */
+			else
+				printf("  ");/*empty print two space */
+			if (i % 2)
+				printf(" ");/*if even give a space*/
+		}
+		for (i = 0; i < j; i++)
+		{
+			int a = b[k + i];/*if not btw space and end of a-z*/
+
+			if (a < 32 || a > 122)
+				a = '.';
+			printf("%c", a);/*print full-stop*/
+		}
+		printf("\n");
+	}
 }
