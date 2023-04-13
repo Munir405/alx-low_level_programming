f9d78ffe8d307c0ee3b06cd038ff5e5949867efa
akubosylva <akubosylva@gmail.com>
2023-04-13 22:07:39 +0100

    Linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -2,6 +2,8 @@
 
 /**
  * linear_search - search linearly
+ * Description: linear search
+ * @i++: increamental
  * @array: given array of ints
  * @size: size of array
  * @value: value to search for
@@ -15,16 +17,6 @@ int linear_search(int *array, size_t size, int value)
 	if (!array)
 		return (-1);
 
-/**
- * i = 0;
- *	while (i < size)
- *	{
- *		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
- *		if (array[i] == value)
- *			return (i);
- *		i++;
- *	}
- */
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
